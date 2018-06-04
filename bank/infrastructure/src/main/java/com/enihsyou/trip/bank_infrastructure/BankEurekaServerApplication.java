ae22be8f41aa3b5180c54015fd842603f3abb812
九条涼果 <s1131234@gmail.com>
2018-06-05 00:16:57 +0800

    添加基础项目结构

---
+++ bank/infrastructure/src/main/java/com/enihsyou/trip/bank_infrastructure/BankEurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.enihsyou.trip.bank_infrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Profile;
+
+@Profile("eureka-server")
+@SpringBootApplication
+@EnableEurekaServer
+public class BankEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankEurekaServerApplication.class, args);
+    }
+}
